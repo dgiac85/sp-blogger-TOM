10f215713ce128104239e8e9def7a93fde0e44e2
dgiac85 <dgiac85@gmail.com>
2015-11-09 18:28:56 +0100

    new things

---
+++ app/modules/admin/adminModule.js
@@ -0,0 +1,31 @@
+'use strict'
+
+angular.module('spBlogger.admin',['spBlogger.admin.controllers','spBlogger.admin.services']);
+
+angular.module('spBlogger.admin').config(['$stateProvider',function($stateProvider){
+	$stateProvider.state('admin',{
+		url:'/admin',
+		abstract:true,
+		controller:'AdminController',
+		templateUrl:'modules/admin/views/admin-home.html'
+	});
+
+	$stateProvider.state('admin.postNew',{
+		url:'/posts/new',
+		controller: 'PostCreationController',
+		templateUrl:'modules/admin/views/admin-new-post.html'
+	});
+
+	$stateProvider.state('admin.postUpdate',{
+		url:'/posts/:id/edit',
+		controller: 'PostUpdateController',
+		templateUrl:'modules/admin/views/admin-update-post.html'
+	});
+
+	$stateProvider.state('admin.postViewAll',{
+		url:'',
+		controller: 'PostListController',
+		templateUrl:'modules/admin/views/admin-all-posts.html'
+	});
+	
+}]);
